bea333833701a9b76ccdd3ec24413cf4d38cc2f4
AmilMasic <amil.masic@gmail.com>
2021-06-25 13:27:10 -0600

    stubbed out blog controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,37 @@
 class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+    render json: @blogs
+  end
+
+  def create
+
+
+    @blog = @user.blogs.new(blog_params)
+    if @blog.save
+      render json: @user
+    else
+      render json: { error: 'Error creating blog.'}
+    end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+    render json: @blog
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @user = User.find(@blog.user_id)
+    @blog.destroy
+    render json: @user
+  end
+
+  private
+
+
+  def blog_params
+    params.require(:blog).permit(:title, :content, :user_id)
+  end
+
 end
